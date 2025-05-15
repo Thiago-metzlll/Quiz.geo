a189124b19b0604c2083fc4d0abbd36131d9a1f3
Thiago-metzlll <thiagostopassola@gmail.com>
2025-05-15 20:53:31 -0300

    att

--- controller/index.js
+++ controller/index.js
@@ -26,7 +26,51 @@ document.addEventListener('DOMContentLoaded', () => {
     },
      {
       pergunta: "Qual é a capital de Curitiba?",
-      respostas: ["Paraná", "São Paulo", "Rússia Br", "Todas as acima"],
+      respostas: ["Paraná", "São Paulo", "Rússia Br", "Todas as acima", "sla mn"],
+      correta: 4
+    },
+    
+     {
+      pergunta: "Quantas capitais possue a África do Sul?",
+      respostas: ["uma", "nenhuma", "duas", "três"],
+      correta: 3
+    },
+    
+     {
+      pergunta: "a União Européia é?",
+      respostas: ["um país", "um estado", "um ente supranacional", "Mercosul"],
+      correta: 2
+    },
+    {
+      pergunta: "Qual país possui formato similar ao Paraná?",
+      respostas: ["Guiana Brasileira", "Romênia", "Chad", "Rússia"],
+      correta: 1
+    },
+    {
+      pergunta: "Quem possui uma bandeira com armas?",
+      respostas: ["Moçambique", "França", "Itália", "Colômbia"],
+      correta: 0
+    },
+    {
+      pergunta: "A Califórnia possui um Pib?",
+      respostas: ["maior que de SP", "menor que o Texas", "igual ao da Colômbia", "maior que o Brasil"],
+      correta: 3
+    },
+
+    {
+      pergunta: "a capital da Austrália é?",
+      respostas: ["Sidney", "Camberra", "Melboune", "Auckland"],
+      correta: 1
+    },
+
+    {
+      pergunta: "o pais mais rico em PIB nominal por PPC é",
+      respostas: ["México", "EUA", "China", "Lesoto"],
+      correta: 2
+    },
+    {
+    pergunta: "A língua mais próxima ao português é",
+      respostas: ["Espanhol", "Galego", "Italiano", "português arcaico da Guiana brasileira"],
       correta: 1
     },
   ];
